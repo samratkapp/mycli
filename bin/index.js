6300c8a29eff9bdf463cbc1c469c1391008ee92d
samratkapp <samsweb007@gmail.com>
2024-03-24 23:18:10 +0530

    .

---
+++ bin/index.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+// The first line that begins with #! is usually called a “shebang.” This is normally only used on Linux or UNIX operating systems to inform the system what type of script is included in the rest of the text file. However, this first line is also required for Node.js scripts to be installed and run properly on macOS and Windows.
+// The shebang line tells the operating system which interpreter to use to parse the remainder of a file or a script.
+
+// https://developer.okta.com/blog/2019/06/18/command-line-app-with-nodejs
+
+console.log("Hello World!!")
